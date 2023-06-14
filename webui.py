abfdcc13e073fb4b8de605733fcebc3b5bd9899a
Bagasekaaprs <136058556+21sysai@users.noreply.github.com>
2023-06-14 12:45:45 +0700

    Update webui.py

--- webui.py
+++ webui.py
@@ -80,13 +80,13 @@ else:
 def check_rollback_vae():
     if shared.cmd_opts.rollback_vae:
         if not torch.cuda.is_available():
-            log.error("Rollback VAE functionality requires compatible GPU")
+            #log.error("Rollback VAE functionality requires compatible GPU")
             shared.cmd_opts.rollback_vae = False
         elif not torch.__version__.startswith('2.1'):
-            log.error("Rollback VAE functionality requires Torch 2.1 or higher")
+            #log.error("Rollback VAE functionality requires Torch 2.1 or higher")
             shared.cmd_opts.rollback_vae = False
         elif 0 < torch.cuda.get_device_capability()[0] < 8:
-            log.error('Rollback VAE functionality device capabilities not met')
+            #log.error('Rollback VAE functionality device capabilities not met')
             shared.cmd_opts.rollback_vae = False
 
 def fix_asyncio_event_loop_policy():
